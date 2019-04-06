c4a3eab259a8ecb191e30f68a14a4a3de60ff86b
Aishwarya Shrestha <asworeya2shrestha@gmail.com>
2019-04-06 23:54:09 +0545

    CRUD-USERS-API: Redefine user schema

--- src/routes.ts
+++ src/routes.ts
@@ -1,18 +1,21 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import user from './routes/user';
-import * as homeController from './controllers/home';
+import user from "./routes/user";
+import profile from "./routes/profile";
+
+import * as homeController from "./controllers/home";
 
 const router: Router = Router();
 
 /**
  * GET /api
  */
-router.get('/', homeController.index);
+router.get("/", homeController.index);
 
 /**
  * GET /api/users
  */
-router.use('/users', user);
+router.use("/users", user);
+router.use("/profile", profile);
 
 export default router;
